904c957ce87b686a66f591903d7aa063f8a62507
Ken Lu <ken@seeice.com>
2014-02-22 14:27:31 -0500

    some more spec

--- spec/my_mongoid/field_spec.rb
+++ spec/my_mongoid/field_spec.rb
@@ -1,4 +1,6 @@
 # spec/my_mongoid/field_spec.rb
+#require File.expand_path('../spec_helper', __FILE__)
+require_relative '../spec_helper'
 describe MyMongoid::Field do
   it "is a module" do
     expect(MyMongoid::Field).to be_a(Module)
--- spec/my_mongoid/spec_helper.rb
+++
@@ -1,1 +0,0 @@
-require "../lib/my_mongoid"
